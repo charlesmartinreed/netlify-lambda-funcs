fa318d54eb3e961ab7081c4cd4fb871038b94e37
charlesmartinreed <charlesmartinreed@icloud.com>
2019-05-30 17:50:05 -0500

    Make GET call to github users API.

--- functions/getUsers.js
+++ functions/getUsers.js
@@ -1,9 +1,34 @@
-// 
+// https://api.github.com/users?client_id=d3537cbd5d58f03bb88a&client_secret=790f16aad8de5b2087949e4743feeea8e92540f0
+const axios = require("axios");
+
 exports.handler = function(event, content, callback) {
-    const { name } = JSON.parse(event.body);
-    //returns to the user, takes possible error, and res to be sent to the user
+  const API_URL = "https://api.github.com/users";
+  const API_CLIENT_ID = "d3537cbd5d58f03bb88a";
+  const API_CLIENT_SECRET = "790f16aad8de5b2087949e4743feeea8e92540f0";
+
+  const URL = `${API_URL}?client_id=${API_CLIENT_ID}&client_secret=${API_CLIENT_SECRET}`;
+
+  // send response to user
+  const send = body => {
+    console.log("sendinggggggggggggggggg");
     callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({msg: `Hello, ${name}`})
-    })
-}
+      statusCode: 200,
+      body: JSON.stringify(body)
+    });
+  };
+
+  // Perform API call to Github
+  const getUsers = async () => {
+    try {
+      const res = await axios.get(URL);
+      send(res.data);
+    } catch (err) {
+      send(err);
+    }
+  };
+
+  // Only GET requests are valid
+  if (event.httpMethod == "GET") {
+    getUsers();
+  }
+};
